85181bf4ac378d727f0872635e6a25b4c819a8d6
David Gallego Gomez <davizs@hotmail.com>
2019-05-24 20:28:55 +0200

    Terminando la iteración 5

--- starter-code/components/Message.js
+++ starter-code/components/Message.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 const Message = (props) => (
-  <article className="message">
-    <div className="message-header">
-      <p>Hello World</p>
-      <button className="delete" aria-label="delete"></button>
-    </div>
-    <div className="message-body">
-      props.children
-    </div>
-  </article>
+  <div className="container">
+   <article className="message">
+      <div className="message-header">
+        <p>Hello World</p>
+        <button className="delete" aria-label="delete"></button>
+      </div>
+      <div className="message-body">
+        {props.children}
+      </div>
+    </article>
+  </div>
 )
 
 export default Message
